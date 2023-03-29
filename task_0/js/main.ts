ad76a123b19ce55ac94a6c9eef4c89a5555fc200
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-29 18:50:50 -0300

    task 0 fix

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -18,7 +18,7 @@ const student2:  Student = {
     location: "LaCasita"
 }
 
-const students: Array<Student> = [student1, student2]
+const studentsList: Array<Student> = [student1, student2]
 
 const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
 const table: HTMLTableElement = document.createElement("table");
@@ -32,7 +32,7 @@ cell1Head.innerHTML = "firstName";
 cell2Head.innerHTML = "location";
 table.append(thead);
 
-students.forEach((student) => {
+studentsList.forEach((student) => {
     const row: HTMLTableRowElement = tbody.insertRow(0);
     const cell1: HTMLTableCellElement = row.insertCell(0);
     const cell2: HTMLTableCellElement = row.insertCell(1);
