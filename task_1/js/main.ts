acde4ad357bcc6e89f2d4f9a88ebf56a19f5e6dd
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-31 20:10:08 -0300

    task4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -16,3 +16,25 @@ export const printTeacher: printTeacher = (firstName: string, lastName: string):
 interface printTeacher {
     (firstName: string, lastName: string): string
 }
+
+interface StudentClassConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return this.firstName;
+    }
+}
